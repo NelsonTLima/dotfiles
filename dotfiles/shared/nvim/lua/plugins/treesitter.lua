1e2421b2bedf14d0998f8db348a34617e2c893be
Nelson Lima <nelsontlima@gmail.com>
2026-06-06 20:21:15 -0300

    Update nvim-treesitter

--- dotfiles/shared/nvim/lua/plugins/treesitter.lua
+++ dotfiles/shared/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,23 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "arduino", "bash", "c", "c_sharp", "cmake", "cpp", "css",
-        "csv", "dockerfile", "git_config", "git_rebase", "gitattributes",
-        "gitcommit", "gitignore", "html", "java", "javascript", "json",
-        "lua", "nginx", "passwd", "php", "python", "r", "ruby",
-        "rust", "sql", "ssh_config", "typescript", "vim", "xml", "vue", "yaml",
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = { enable = true },
+    require("nvim-treesitter").install({
+      "arduino", "bash", "c", "c_sharp", "cmake", "cpp", "css",
+      "csv", "dockerfile", "git_config", "git_rebase", "gitattributes",
+      "gitcommit", "gitignore", "html", "java", "javascript", "json",
+      "lua", "markdown", "markdown_inline","nginx","passwd", "php",
+      "python", "r", "ruby", "rust", "sql", "ssh_config", "typescript",
+      "vim", "xml", "vue", "yaml",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
